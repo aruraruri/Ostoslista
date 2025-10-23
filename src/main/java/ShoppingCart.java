f5c234d4a9adbad50c70328ab9b18af6617e4167
aruraruri <aurilait@gmail.com>
2025-10-23 14:02:23 +0300

    Add localization for 4 languages, add main program logic that asks for eggs and socks.

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class ShoppingCart {
@@ -6,18 +8,27 @@ public class ShoppingCart {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // ask lang
+        System.out.println("Enter language: ");
+        String lang = sc.nextLine();
+        System.out.println("Enter country: ");
+        String country = sc.nextLine();
+        Locale locale = new Locale(lang, country);
+        ResourceBundle messagesBundle = ResourceBundle.getBundle("MessagesBundle", locale);
+
+
         int eggAmt = 0;
         double eggPrice = 0.0;
-        System.out.println("Enter amount of eggs");
+        System.out.println(messagesBundle.getString("prompt.eggAmt"));
         eggAmt = sc.nextInt();
-        System.out.println("Enter price of eggs");
+        System.out.println(messagesBundle.getString("prompt.eggPrice"));
         eggPrice = sc.nextDouble();
 
         int sockAmt = 0;
         double sockPrice = 0.0;
-        System.out.println("Enter amount of socks");
+        System.out.println(messagesBundle.getString("prompt.sockAmt"));
         sockAmt = sc.nextInt();
-        System.out.println("Enter price of socks");
+        System.out.println(messagesBundle.getString("prompt.sockPrice"));
         sockPrice = sc.nextDouble();
 
         total = total + ((eggAmt*eggPrice) + (sockAmt*sockPrice));
---
+++ src/test/java/ResourceBundleTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.Assert.assertEquals;
+
+public class ResourceBundleTest {
+
+    @Test
+    void testEnglishBundle() {
+    //ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", new Locale("en", "US"));
+    //Assertions.assertEquals("enUS", rb.getString("lang.eng"));
+    }
+
+}
